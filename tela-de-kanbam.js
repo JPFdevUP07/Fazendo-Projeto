16bee89bb809ee7b45e1bc9ddf108c33f7096491
JPFdevUP07 <joaopedrogh07@gmail.com>
2024-08-26 16:33:42 -0300

    atualizações do projteto

--- tela-de-kanbam.js
+++ tela-de-kanbam.js
@@ -1,21 +1,57 @@
 const $modal = document.getElementById('modal');
 const $descriptionInput = document.getElementById('description');
 const $priorityInput = document.getElementById('priority');
-const $deadLineInput = documen.getElementById('deadline');
+const $deadLineInput = document.getElementById('deadline');
 
+const $todoColumnBody = document.querySelector('#todoColumn .body');
 
-function openModal(){
+var todoList = [];
+
+function openModal(){   /*abrir modal*/
     $modal.style.display = "flex";
     document.body.style.overflow = "hidden";
 }
 
-function closeModal(){
+function closeModal(){ /*fechar modal */
     $modal.style.display = "none";
     document.body.style.overflow = "auto";
 }
 
-function createTask() {
-    console.log($descriptionInput.value);
-    console.log($priorityInput.value);
-    console.log($deadLineInput.value);
+function generateCards (){ /*gerar cards */
+    const todoListhtml = todoList.map(function(task){
+        return `
+            <div class="card">
+                <div class="info">
+                    <b>Descrição</b>
+                    <span>${task.description}</span>
+                </div>
+
+                 <div class="info">
+                    <b>Prioridade</b>
+                    <span>${task.priority}</span>
+                </div>
+
+                 <div class="info">
+                    <b>Prazo</b>
+                    <span>${task.deadLine}</span>
+                </div>
+            </div>
+        `;
+    })
+
+    $todoColumnBody.innerHTML = todoListhtml.join('');
 }
+
+function createTask(){ /*criando tasks */
+    const newTask = {
+        description: $descriptionInput.value,
+        priority: $priorityInput.value,
+        deadLine: $deadLineInput.value,
+    }
+
+    todoList.push(newTask);
+
+    closeModal();
+    generateCards();
+}
+
