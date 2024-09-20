d18b9539b77530ca1aa2e76bfc87dac626c7a308
JPFdevUP07 <joaopedrogh07@gmail.com>
2024-09-20 16:37:42 -0300

    atualizações

--- tela-de-kanbam.js
+++ tela-de-kanbam.js
@@ -14,36 +14,43 @@ const $editingModeButton = document.getElementById('editingModeButton');
 
 var taskList = [];
 
-function openModal(id){   /*abrir modal*/
+function openModal(data_column){   /*abrir modal*/
     $modal.style.display = "flex";
     document.body.style.overflow = "hidden";
 
-    if(id) {
-        $criationModeTitle.style.display = "none";
-        $editingModeTitle.style.display = "block";
+    $criationModeTitle.style.display = "block";
+    $editingModeTitle.style.display = "none";
 
-        $crationModeButton.style.display = "none";
-        $editingModeButton.style.display = "block";
-
-        const index = taskList.findIndex(function(task) {
-            return task.id == id;
-        });
-
-        const task = taskList[index];
-
-        $idInput.value = task.id;
-        $descriptionInput.value = task.description;
-        $priorityInput.value = task.priority;
-        $deadLineInput.value = task.deadLine;
-        $columnInput.value = task.column;
-    } else {
-        $criationModeTitle.style.display = "block";
-        $editingModeTitle.style.display = "none";
-
-        $crationModeButton.style.display = "block";
-        $editingModeButton.style.display = "none";
-    }      
+    $crationModeButton.style.display = "block";
+    $editingModeButton.style.display = "none";
+          
 }
+
+function openModalToEdit(id){   /*abrir modal para editar*/
+    $modal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+
+   
+    $criationModeTitle.style.display = "none";
+    $editingModeTitle.style.display = "block";
+
+    $crationModeButton.style.display = "none";
+    $editingModeButton.style.display = "block";
+
+    const index = taskList.findIndex(function(task) {
+        return task.id == id;
+    });
+
+    const task = taskList[index];
+
+    $idInput.value = task.id;
+    $descriptionInput.value = task.description;
+    $priorityInput.value = task.priority;
+    $deadLineInput.value = task.deadLine;
+    $columnInput.value = task.column;
+       
+}
+
 
 function closeModal(){ /*fechar modal */
     $modal.style.display = "none";
@@ -56,43 +63,45 @@ function closeModal(){ /*fechar modal */
     $columnInput.value = "";
 }
 
-function resetColumns (){
-    document.querySelector('[data-column"1"] .body').innerHTML = '';
-    document.querySelector('[data-column"2"] .body').innerHTML = '';
-    document.querySelector('[data-column"3"] .body').innerHTML = '';
-    document.querySelector('[data-column"4"] .body').innerHTML = '';
+function resetColumns() {
+    document.querySelector('[data-column="1"] .body').innerHTML = "";
+    document.querySelector('[data-column="2"] .body').innerHTML = "";
+    document.querySelector('[data-column="3"] .body').innerHTML = "";
+    document.querySelector('[data-column="4"] .body').innerHTML = "";
 }
 
-function generateCards (){ /*gerar cards */
-
+function generateCards() {
+    // Chama o resetColumns uma vez, antes de começar a gerar os cards
     resetColumns();
-    
-    taskList.forEach(function(task){
 
-        const columnBody = document.querySelector(`[data-column = "${task.column}"] .body`);
+    // Itera sobre a lista de tarefas para gerar os cards
+    taskList.forEach(function (task) {
+        const columnBody = document.querySelector(`[data-column="${task.column}"] .body`);
 
         const card = `
-            <div class="card" ondblclick="openModal(${task.id})">
+            <div class="card" ondblclick="openModalToEdit(${task.id})">
                 <div class="info">
                     <b>Descrição</b>
                     <span>${task.description}</span>
                 </div>
 
-                 <div class="info">
+                <div class="info">
                     <b>Prioridade</b>
                     <span>${task.priority}</span>
                 </div>
 
-                 <div class="info">
+                <div class="info">
                     <b>Prazo</b>
                     <span>${task.deadLine}</span>
                 </div>
             </div>
         `;
-
         columnBody.innerHTML += card;
     });
 }
+
+
+
 
 function createTask(){ /*criando tasks */
     const newTask = {
@@ -101,7 +110,7 @@ function createTask(){ /*criando tasks */
         priority: $priorityInput.value,
         deadLine: $deadLineInput.value,
         column: $columnInput.value,
-    }
+    };
 
     taskList.push(newTask);
 
@@ -117,7 +126,7 @@ function updateTask(){
         priority: $priorityInput.value,
         deadLine: $deadLineInput.value,
         column: $columnInput.value,
-    }
+    };
 
     
     const index = taskList.findIndex(function(task) {
